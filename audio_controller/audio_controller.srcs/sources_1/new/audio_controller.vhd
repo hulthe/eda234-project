93e83f6f1dafbc0b85b427cd0d25ad416955806f
Rongpeng666 <77685192+Rongpeng666@users.noreply.github.com>
2021-12-10 11:27:05 +0100

    Update audio_controller.vhd

--- audio_controller/audio_controller.srcs/sources_1/new/audio_controller.vhd
+++ audio_controller/audio_controller.srcs/sources_1/new/audio_controller.vhd
@@ -10,7 +10,7 @@ entity audio_controller is
         clk : in std_logic;
         reset_n: in std_logic;
         audio_out : out std_logic;
-        trigger: in std_logic
+        trigger: in std_logic_vector (2 downto 0)
     );
 
 end audio_controller;
@@ -18,16 +18,18 @@ end audio_controller;
 
 architecture Behavioral of audio_controller is
     constant frequency: integer := 2268;
+   -- constant ban_const: integer := 191160;  --largest ban limitation number of 191160  flawless_victory.raw
 
     signal cnt_audio: integer range 0 to frequency;  
+  --  signal cnt_ban_trigger: integer range 0 to ban_const; 
     signal pwm_duty_cycle: unsigned(7 downto 0) := (others => '0');
     
-    signal enable_sound: std_logic := '1';
+    signal enable_sound: std_logic := '0';
     
     constant ADDR_SIZE: integer := 19;
     
     -- TODO: move this to a port
-    signal sound_selector: sound := blaster;
+    signal sound_selector: sound;
     
     signal sound_data: std_logic_vector(7 downto 0);
     signal sound_addr: std_logic_vector(ADDR_SIZE-1 downto 0);
@@ -44,7 +46,8 @@ architecture Behavioral of audio_controller is
     signal sound_end: unsigned(ADDR_SIZE-1 downto 0);
     signal playing: std_logic := '0';
     
-    signal last_trigger: std_logic := '0';
+    signal trigger_sig:  std_logic_vector(2 downto 0) := "000";
+    signal last_trigger: std_logic_vector(2 downto 0) := "000";
 
     component blaster_sound_mem
     port (
@@ -66,17 +69,38 @@ architecture Behavioral of audio_controller is
 
 begin
 
+process(clk, reset_n)begin
+    if reset_n = '0' then
+        trigger_sig <= "000";
+    elsif rising_edge(clk) then
+        if playing = '0' then    --playing = '0' means the speaker is not displaying to make sure not new trigger signal interupt the displaying trigger signal
+            trigger_sig <= trigger;
+        end if;
+    end if;
+end process;
+
+--select the kind of sound by trigger_sig
+     with trigger_sig select
+        sound_selector <= blaster when "001",
+                          victory when "010",
+                          game_over when "100",
+                          game_over when others;
+
+--select the sound start addr by sound_selector
     with sound_selector select
         sound_start <=
             to_unsigned(0, ADDR_SIZE) when blaster,
             to_unsigned(34792, ADDR_SIZE) when victory,
             to_unsigned(225952, ADDR_SIZE) when game_over;
+            
+--select the sound end addr by sound_selector
     with sound_selector select
         sound_end <=
             to_unsigned(34792, ADDR_SIZE) when blaster,
             to_unsigned(225952, ADDR_SIZE) when victory,
             to_unsigned(377370, ADDR_SIZE) when game_over;
 
+--output pwm signal
     pwm_ctrl: pwm
         port map(
             clk => clk,
@@ -84,7 +108,8 @@ begin
             duty_cycle => pwm_duty_cycle,
             pwm_out => audio_out
         );
-    
+        
+--BRAM of sound file  
     blaster_sound: blaster_sound_mem
         port map(
             clka => clk,
@@ -93,9 +118,24 @@ begin
             douta => sound_data
         );
 
+--decide on the pwm_duty_cycle by the sound_data from BRAM
     pwm_duty_cycle <= unsigned(sound_data) when playing = '1' else
                       "00000000";
 
+--enable the BRAM reading when playing = '1'
+process(clk, reset_n)begin
+    if reset_n = '0' then
+        enable_sound <= '0';
+    elsif rising_edge(clk) then
+        if playing = '1' then
+            enable_sound <= '1';
+        else 
+            enable_sound <= '0';
+        end if;
+    end if;
+end process;
+
+--cnt for the BRAM read addr plus 1
 process(clk, reset_n)begin
     if reset_n = '0' then
         cnt_audio <= 0;
@@ -108,6 +148,7 @@ process(clk, reset_n)begin
     end if;
 end process;
 
+--control the next_addr and next_playing to control the addr from start arrd to end addr and sound playing flag
 process(clk, reset_n, sound_addr, trigger, cnt_audio)
     variable next_addr: unsigned(ADDR_SIZE-1 downto 0);
     variable next_playing: std_logic;
@@ -117,11 +158,11 @@ begin
         next_playing := '0';
     elsif rising_edge(clk) then
         -- handle trigger
-        if trigger = '1' and last_trigger = '0' then
+        if (trigger_sig = "001" or trigger_sig = "010" or trigger_sig = "100") and last_trigger = "000" then
             next_addr := SOUND_START;
             next_playing := '1';
         end if;
-        last_trigger <= trigger;
+        last_trigger <= trigger_sig;
         
         -- audio player
         if cnt_audio = frequency then
